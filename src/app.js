d0202b83ddc25b6519108ab7a593fcacebe0b2d1
ellyhariati <ellyhariati8@gmail.com>
2023-12-15 11:34:42 -0800

    membuat website brawijaya florist

--- src/app.js
+++ src/app.js
@@ -14,5 +14,88 @@ document.addEventListener("alpine:init", () => {
             { id: 11, name: "Tropical Paradise Bouquets", img: "k2.jpg", price: 940000 },
             { id: 12, name: "Enchanting Roses Ensemble", img: "ee.jpg", price: 999000 },
         ],
+
+        showItemDetailModal(item) {
+            // show the modal based on item details
+            // you can use a modal library or handle the modal visibility directly in Alpine.js
+            // for simplicity, you can add a class to make it visible
+            const modalIndex = this.items.indexOf(item);
+            const modal = document.getElementById(`item-detail-modal-${modalIndex}`);
+
+            // update modal content based on the selected item
+            // example: update the modal content with item details
+            modal.querySelector(".product-content h3").textContent = item.name;
+            modal.querySelector(".product-content p").textContent = `Price: ${rupiah(item.price)}`;
+
+        //show the modal 
+        modal.classList.add("visible");
+        }
     }));
+
+    Alpine.store("cart", {
+        items: [],
+        total: 0,
+        quantity: 0,
+        add(newItem) {
+            // cek apakah ada barang yang sama di cart
+            const cartItem = this.items.find((item) => item.id === newItem.id);
+
+            // jika belum ada / cart masih kosong
+            if(!cartItem) {
+                this.items.push({...newItem, quantity: 1, total: newItem.price });
+                this.quantity++;
+                this.total += newItem.price;
+            } else {
+                // jika barang sudah ada, cek apakah barang beda atau sama dengan yang ada di cart
+                this.items = this.items.map((item) => {
+                    // jika barang berbeda
+                    if (item.id !== newItem.id) {
+                        return item;
+                    } else {
+                        // jika barang sudah ada, tambah quantity dan totalnya
+                        item.quantity++;
+                        item.total = item.price * item.quantity;
+                        this.quantity++;
+                        this.total += item.price;
+                        return item;
+                    }
+                });
+            }
+        },
+        remove(id) {
+            // ambil item yang mau diremove berdasarkan id nya
+            const cartItem = this.items.find((item) => item.id === id);
+
+            // jika item lebih dari satu
+            if(cartItem.quantity > 1) {
+                // telusuri 1 1
+                this.items = this.items.map((item) => {
+                    // jika bukan barang yang diklik
+                    if(item.id !== id) {
+                        return item;
+                    } else {
+                        item.quantity--;
+                        item.total = item.price * item.quantity;
+                        this.quantity--;
+                        this.total -= item.price;
+                        return item;
+                    }
+                });
+            } else if (cartItem.quantity === 1) {
+                // jika barangnya sisa 1
+                this.items = this.items.filter((item) => item.id !== id);
+                this.quantity--;
+                this.total -= cartItem.price;
+            }
+        },
+    });
 });
+
+// konversi ke Rupiah
+const rupiah = (number) => {
+    return new Intl.NumberFormat("id-ID", {
+    style: 'currency',
+    currency: 'IDR',
+    minimumFractionDigits: 0,
+}).format(number);
+};
